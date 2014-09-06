9fe24ed2ffc6ddfbced71bb72191a090c7d697c7
Stan Schwertly (Mac) <stan@schwertly.com>
2014-09-06 19:41:29 -0400

    add some perldoc

--- shortenize.pl
+++ shortenize.pl
@@ -33,3 +33,36 @@ sub shorten {
 
 	return $shortener_url . $alias;
 }
+
+__END__
+
+=head1 shortenize
+
+Takes STDIN, replaces any URLs with shortened ones, prints back out.
+
+=head1 USAGE
+
+	echo "my website is http://schwertly.com" | ./shortenize  
+	./shortenize <file_with_long_urls >file_with_short_ones 
+
+=head1 SYNOPSIS
+
+	./shortenize 
+
+	Options:
+		none
+
+=head1 DESCRIPTION
+
+shortenize takes input on STDIN and replaces all URLs with short ones. I
+use it to make my IRC channel of NYT feeds from being unreadable.
+
+=head1 DEPENDENCIES
+
+rss2text is written in perl and uses LWP::UserAgent to shorten links, and
+URL::Find::Simple to locate the URLs on STDIN.
+
+=head1 AUTHOR
+
+Stan Schwertly (http://www.schwertly.com)
+
